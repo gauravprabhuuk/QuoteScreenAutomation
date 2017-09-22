b9fe00021281a2e5c81561b6ac300a32d3fa3918
gauravprabhuuk <31275450+gauravprabhuuk@users.noreply.github.com>
2017-09-22 10:42:22 +0100

    Updated the files

---
+++ Valtech/src/utility/Constant.java
@@ -0,0 +1,11 @@
+package utility;
+
+public class Constant {
+
+	 public static final String URL = "https://www.moneysupermarket.com/shop/car-insurance/questionset/#?step=highimpactquestions";
+	 
+     public static final String Path_TestData = "D:\\Users\\gauprabh\\Documents\\Java\\Valtech\\src\\testdata\\";
+
+     public static final String File_TestData = "TestData.xlsx";
+	
+}
